ef9dd723e1f2417f0d99d3ff612bd50cca393fc9
Sridevi <sridevi.miriyala@modusbox.com>
2018-06-02 00:22:37 -0400

    Added license information

--- contract-tests/src/test/java/stepdefs/participants/AddParticipantStepdefs.java
+++ contract-tests/src/test/java/stepdefs/participants/AddParticipantStepdefs.java
@@ -1,3 +1,25 @@
+/*****
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Sridevi Miriyala <sridevi.miriyala@modusbox.com>
+ --------------
+ ******/
+
 package stepdefs.participants;
 
 import com.mojaloop.utils.MLResponse;
@@ -70,7 +92,12 @@ public class AddParticipantStepdefs extends SpringAcceptanceTest {
 
     @Given("^a participant with MSISDN \"([^\"]*)\" exists in switch with a \"([^\"]*)\"$")
     public void aParticipantExistsInSwitchWithA(String id, String fspId) throws Throwable {
-        response = Utility.get(participantsBaseUrl + "/participants/MSISDN/" + id ,fspId,null,null,getRestTemplate());
+        Map<String,String> headers = new HashMap<>();
+        headers.put("Accept","");
+        headers.put("FSPIOP-Source","payerfsp");
+
+        response = Utility.get(String.join(participantsBaseUrl).join("/","MSISDN").join("/", id) ,headers,null,getRestTemplate());
+
         assertThat(response.getResponseBody(),containsString(fspId));
     }
 
@@ -222,7 +249,6 @@ public class AddParticipantStepdefs extends SpringAcceptanceTest {
         headers.put("FSPIOP-Source","payerfsp");
 
         response = Utility.post(String.join(participantsBaseUrl).join("/","MSISDN").join("/", id),requestJson,headers, null,getRestTemplate());
-
 
     }
 
